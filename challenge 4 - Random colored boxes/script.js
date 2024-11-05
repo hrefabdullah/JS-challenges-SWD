1acd724e778c291799e956bfc6e61011d6b00748
Abdullah <aa3541900@gmail.com>
2024-11-05 14:36:03 +0530

    challenge 4 - Solution

---
+++ challenge 4 - Random colored boxes/script.js
@@ -0,0 +1,70 @@
+function color(){
+    var randomColor = Math.random()
+    if (randomColor < 0.2){
+        return "purple" 
+    } else if (randomColor >= 0.2 & randomColor < 0.4){
+        return "green"
+    } else if (randomColor >= 0.4 & randomColor < 0.6){
+        return "yellow"
+    } else if (randomColor >= 0.6 & randomColor < 0.8){
+        return "blue"
+    } else {
+        return 'red'
+    }    
+}
+function bg(){
+    var randomBg = Math.random()
+    if (randomBg < 0.2){
+        return "orange" 
+    } else if (randomBg >= 0.2 & randomBg < 0.4){
+        return "lightblue"
+    } else if (randomBg >= 0.4 & randomBg < 0.6){
+        return "darkblue"
+    } else if (randomBg >= 0.6 & randomBg < 0.8){
+        return "brown"
+    } else {
+        return 'white'
+    }
+}
+
+var color1 = color()
+var bg1 = bg()
+document.getElementById("box1").style.cssText = `
+    color: ${color1};
+    background-color: ${bg1};
+`
+
+var color2 = color()
+var bg2 = bg()
+document.getElementById("box2").style.cssText = `
+    color: ${color2};
+    background-color: ${bg2};
+`
+
+var color3 = color()
+var bg3 = bg()
+document.getElementById("box2").style.cssText = `
+    color: ${color3};
+    background-color: ${bg3};
+`
+
+var color3 = color()
+var bg3 = bg()
+document.getElementById("box3").style.cssText = `
+    color: ${color3};
+    background-color: ${bg3};
+`
+
+var color4 = color()
+var bg4 = bg()
+document.getElementById("box4").style.cssText = `
+    color: ${color4};
+    background-color: ${bg4};
+`
+
+var color5 = color()
+var bg5 = bg()
+document.getElementById("box5").style.cssText = `
+    color: ${color5};
+    background-color: ${bg5};
+`
